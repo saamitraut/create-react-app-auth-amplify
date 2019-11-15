2a702c23d513b6b81123b66a10445939695c7ca3
saamitraut <saamitraut@gmail.com>
2019-11-15 18:49:37 +0530

    some changes.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Vauch
+            Vauch..
           </p>
           <a
             className="App-link"
